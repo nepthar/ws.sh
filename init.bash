a5d52a4df98203a1b208942d82cf32ef944c2e63
Jordan Parker <nepthar@gmail.com>
2025-02-27 15:48:14 -0800

    main: AI improvements

--- init.bash
+++ init.bash
@@ -7,7 +7,6 @@ if [[ -z "$BASH_VERSION" ]]; then
 fi
 
 (return 0 2>/dev/null) && sourced=true || sourced=false
-
 if [[ "$sourced" == "false" ]]; then
   echo "ws.sh: init.bash must be sourced rather than run" >&2
   exit 1
@@ -25,32 +24,105 @@ fi
 if [[ $BASH_SOURCE == *"/ws.sh/init.bash" ]]; then
   ws_root="${BASH_SOURCE%%/init.bash}"
 else
-  echo "ws.sh: Unable to figure out where ws.sh is located. Please" \
-    "provide a full path when sourcing init.bash." \
-    "Got: \"$BASH_SOURCE\". Workspaces cannot be set up." >&2
+  echo "ws.sh: Unable to determine ws.sh location. Please provide a full path when sourcing init.bash. Got: \"$BASH_SOURCE\"" >&2
   return 1
 fi
 
-# Pass the workspace name to subprocesses
-export workspace
-
 # Configuration
+export workspace
 ws_spaces="${ws_root}/known"
 ws_home=
 ws_funcs=()
 ws_file=
 ws_prefix=
 
+# Helper functions
+_ws.is_active() { [[ -n "$workspace" ]]; }
+
+_ws.resolve_file() {
+  local path
+  
+  # No argument - check current directory
+  if [[ -z $1 ]]; then
+    path="./workspace.sh"
+  # Literal workspace file
+  elif [[ "$1" == *"workspace.sh" ]]; then
+    path="$1"
+  # Known workspace
+  elif [[ -L "${ws_spaces}/${1}.ws" ]]; then
+    path="${ws_spaces}/${1}.ws"
+  # Directory containing workspace.sh
+  elif [[ -f "${1}/workspace.sh" ]]; then
+    path="${1}/workspace.sh"
+  else
+    return 1
+  fi
+  
+  # Return absolute path if file exists and is readable
+  [[ -r "$path" ]] && readlink -f "$path" || return 1
+}
+
+_ws.template() {
+  sed -e "s/{NAME}/$1/g" "${ws_root}/workspace.template"
+}
+
+_ws.active() {
+  local cmd="$1"
+  local dot_command="${ws_prefix}.${cmd}"
+
+  # Run without arguments, just cd to workspace root
+  if [[ -z $cmd ]]; then
+    cd "$ws_home"
+    return
+  fi
+
+  shift 1
+
+  if type -t "$dot_command" &>/dev/null; then
+    $dot_command "$@"
+  elif [[ "$cmd" == "help" ]]; then
+    ws.help
+  else
+    echo "Command not found: $cmd" >&2
+    return 1
+  fi
+}
+
+_ws.inactive() {
+  case "$1" in
+    "new") shift; ws.new "$@" ;;
+    "add") ws.add "$2" ;;
+    *) ws.enter "$@" ;;
+  esac
+}
+
+_ws.tab_comp_inactive() {
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  local words=()
+  
+  # Use nullglob to handle case when no files match
+  shopt -s nullglob
+  for file in "${ws_spaces}"/*.ws; do
+    words+=("${file##*/}")
+    words[-1]="${words[-1]%.ws}"
+  done
+  shopt -u nullglob
+  
+  COMPREPLY=($(compgen -W "${words[*]}" -- "$cur"))
+}
+
+# Public functions
 ## ws.info
 ## Dumps information about the workspace state
-ws.info()
-{
-  echo "ws name:   $workspace"
-  echo "ws_root:   $ws_root"
-  echo "ws_home:   $ws_home"
-  echo "ws_funcs:  (${ws_funcs[@]})"
-  echo "ws_file:   $ws_file"
-  echo "ws_prefix: $ws_prefix"
+ws.info() {
+  cat <<EOF
+ws name:   $workspace
+ws_root:   $ws_root
+ws_home:   $ws_home
+ws_funcs:  (${ws_funcs[*]})
+ws_file:   $ws_file
+ws_prefix: $ws_prefix
+EOF
 }
 
 ## Displays the help strings for all available commands
@@ -66,39 +138,31 @@ ws.help() {
 
 ## ws.ls
 ## List known workspaces
-ws.ls()
-{
+ws.ls() {
   echo "Spaces in ${ws_spaces}:"
-  ls "${ws_spaces}"
+  ls -1 "${ws_spaces}"
 }
 
 ## ws.new (name)
 ## Create a new workspace in the current folder with a given name. If
 ## name is not provided, the name of the folder will be used.
-ws.new()
-{
-  local new_name="$1"
-  local new_home="$PWD"
-  local throwaway
+ws.new() {
+  local new_name="${1:-${PWD##*/}}"
   local tail="(enter to continue, ctrl+c to stop)"
-
-  if [[ -z $new_name ]]; then
-    new_name="${new_home##*/}"
-  fi
+  local throwaway
 
   if [[ -f workspace.sh ]]; then
-    echo "workspace.sh already exists"
+    echo "workspace.sh already exists" >&2
     return 1
   fi
 
-  if ! read -r -p "Create Create \"$new_name\" @ ${new_home}? $tail" throwaway; then
+  if ! read -r -p "Create \"$new_name\" @ ${PWD}? $tail" throwaway; then
     echo "canceled."
     return 1
   fi
 
-  _ws.template $new_name > workspace.sh
-
-  echo "$new_name created in $new_file."
+  _ws.template "$new_name" > workspace.sh
+  echo "$new_name created in workspace.sh"
 
   read -r -p "Enter $new_name? $tail" throwaway || return 0
   ws.enter
@@ -109,68 +173,52 @@ ws.new()
 
 ## ws.enter (name/filename)
 ## Enter into a workspace. If (name/filename) is provided, it will be looked up
-## in linked workspaces. If not, the workspace in the current dir will
-## be used.
-ws.enter()
-{
+## in linked workspaces. If not, the workspace in the current dir will be used.
+ws.enter() {
   local possible_file
 
   if _ws.is_active; then
-    echo "Already in workspace: $workspace"
+    echo "Already in workspace: $workspace" >&2
     return 1
   fi
 
-  # This will either resolve to the full path of a readable file or fail.
+  # Resolve workspace file path
   if ! possible_file="$(_ws.resolve_file "$1")"; then
-    echo "Unable to resolve workspace from \"$1\""
+    echo "Unable to resolve workspace from \"$1\"" >&2
     return 1
   fi
 
   ws_file="$possible_file"
   ws_home="${ws_file%/*}"
 
-  # There's very little we can do to see if we entered a "valid" workspace
-  # or just sourced a file. In reality, this is enough as, after all, a
-  # workspace is just a collection of scripts.
-  if ! {
-    cd "$ws_home" &&
-    source "$ws_file" &&
-    test $workspace; # tests if $workspace is not empty
-  } ; then
-    echo \
-      "Failed to enter workspace. Shell is probbably in a bad state" \
-      "and should be closed. Note that workspace files must set \$workspace." \
-      "Additional info:" >&2
+  # Enter the workspace
+  if ! { cd "$ws_home" && source "$ws_file" && [[ -n "$workspace" ]]; }; then
+    echo "Failed to enter workspace. Note that workspace files must set \$workspace." >&2
     ws.info >&2
     return 1
   fi
 
   ws_prefix=${ws_prefix:-$workspace}
 
-  # Sourcing the file & running init seems to have gone OK.
-  # Generate the list of commands for tab complete
-  ws_funcs=($(
-    for funcname in $(declare -F | cut -c12- | grep "^${ws_prefix}\."); do
-      funcname=${funcname#${ws_prefix}.}
-      echo ${funcname#"root."}
-    done
-  ))
+  # Generate command list for tab completion
+  mapfile -t ws_funcs < <(
+    declare -F | 
+    cut -d' ' -f3 | 
+    grep "^${ws_prefix}\." | 
+    sed -e "s/^${ws_prefix}\.//" -e "s/^root\.//"
+  )
 
-
-  # Project entry mapping & tab completion
+  # Set up command alias and tab completion
   alias ,=_ws.active
   complete -W "${ws_funcs[*]}" ,
 
-  # debug:
-  # echo "ws_file=$ws_file, workspace=$workspace, ws_funcs=(${ws_funcs[@]})" >&2
-
-  ws.refresh()
-  {
+  # Define refresh function within the workspace context
+  ws.refresh() {
     unset workspace
     if ws.enter "$ws_file"; then
       echo "Refreshed workspace from ${ws_file}"
     else
-      echo "Failed. Workspace is likely in a broken state, close the terminal"
+      echo "Failed. Workspace is likely in a broken state." >&2
       return 1
     fi
   }
@@ -178,112 +226,20 @@ ws.enter()
 
 ## ws.add
 ## Add a link to the active workspace in $ws_spaces
-ws.add()
-{
+ws.add() {
   if ! _ws.is_active; then
-    echo "You must be in an active workspace to add it to your known spaces"
+    echo "You must be in an active workspace to add it to your known spaces" >&2
     return 1
   fi
 
   local linkname="${ws_spaces}/${workspace}.ws"
-
+  mkdir -p "$ws_spaces"
+  
   echo "Link $workspace: $ws_file -> ${ws_spaces}"
-  ln -s "$ws_file" "$linkname"
+  ln -sf "$ws_file" "$linkname"
 }
 
-_ws.active()
-{
-  # Forward the command
-  local cmd="$1"
-
-  local dot_command="${ws_prefix}.${cmd}"
-
-  # Run without arguments, just cd to the root of the workspace
-  if [[ -z $cmd ]]; then
-    cd "$ws_home"
-    return
-  fi
-
-  shift 1
-
-  if isfunc $dot_command; then
-    $dot_command "$@"
-  elif [[ "$cmd" == "help" ]]; then
-    ws.help
-  else
-    echo "$cmd not found"
-    return 1
-  fi
-}
-
-_ws.inactive()
-{
-  case "$1" in
-    "new")
-      shift 1
-      ws.new "$@"
-      ;;
-    "add")
-      ws.add "$2"
-      ;;
-    *)
-      ws.enter "$@"
-      ;;
-  esac
-}
-
-_ws.is_active()
-{
-  # Check if $workplace is set
-  test $workspace
-}
-
-_ws.tab_comp_inactive()
-{
-  # When not in a workspace, generate a list of known workspaces
-  local cur
-  local words
-
-  cur=${COMP_WORDS[COMP_CWORD]}
-
-  words=$(
-  for file in ${ws_spaces}/*.ws; do
-    f1="${file%.*}"
-    echo "${f1##*/}"
-  done)
-
-  COMPREPLY=($(compgen -W "$words" -- $cur))
-}
-
-# Resolve a token to an absolute file path of a file that exists
-_ws.resolve_file()
-{
-  if [[ -z $1 ]]; then
-    # No argument given - check only if the workspace file
-    # exists right here.
-    readlink -f ./workspace.sh
-    return $?
-  fi
-
-  # See if it's a literal workspace file that exists
-  if [[ "$1" == *"workspace.sh" ]]; then
-    readlink -f "$1" && return 0
-  fi
-
-  # See if it's a known workspace
-  readlink -f "${ws_spaces}/${1}.ws" && return 0
-
-  # See if it's the folder containing a workspace.sh file
-  readlink -f "${1}/workspace.sh" && return 0
-
-  # Otherwise, we cannot resolve
-  return 1
-}
-
-_ws.template() {
-  sed -e "s/{NAME}/$1/g" "${ws_root}/workspace.template"
-}
-
+# Setup
 alias ,=_ws.inactive
 complete -F _ws.tab_comp_inactive ,
-mkdir -p "$ws_root"
+mkdir -p "$ws_spaces"
